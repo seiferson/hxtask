d3208f7b6c7fdc3ece3ed95b3bb25112c4702402
Cuauhtemoc Herrera Muñoz <seifer.ch@gmail.com>
2023-05-06 22:25:27 -0600

    initial spring boot files

---
+++ src/main/java/com/seiferson/hxtask/HxtaskApplication.java
@@ -0,0 +1,13 @@
+package com.seiferson.hxtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HxtaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HxtaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seiferson/hxtask/HxtaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seiferson.hxtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HxtaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
